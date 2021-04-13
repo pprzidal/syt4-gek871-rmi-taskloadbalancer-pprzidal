f46ad440e9251f8aec5c129cfa6d3d28e2f83808
pprzidal <pprzidal@student.tgm.ac.at>
2021-04-13 10:57:13 +0200

    forget to commit Fibonacci.java

---
+++ src/main/java/client/Fibonacci.java
@@ -0,0 +1,40 @@
+package client;
+
+import compute.Task;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+
+public class Fibonacci implements Task<BigInteger>, Serializable {
+    private int enteFibonacci;
+
+    public Fibonacci(int nthFibonacci) {
+        this.enteFibonacci = nthFibonacci;
+    }
+
+    @Override
+    public BigInteger execute() {
+        try {
+            Integer n = enteFibonacci;
+            BigInteger prepre = new BigInteger("0");
+            BigInteger pre = new BigInteger("1");
+            BigInteger out = new BigInteger("1");
+            if (n >= 2) {
+                for (int i = 3; i <= n; i++) {
+                    prepre = pre;
+                    pre = out;
+                    out = prepre.add(pre);
+                }
+                return out;
+                // System.out.println(fibonacci(n));
+            } else if (n >= 0) {
+                return out;
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
