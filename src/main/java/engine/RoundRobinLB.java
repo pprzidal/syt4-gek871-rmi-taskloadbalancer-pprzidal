e3bafa5644279ac29289c1760368abe6ba054960
pprzidal <pprzidal@student.tgm.ac.at>
2021-05-11 11:08:01 +0200

    made RoundRobinLB.java kind off Thread safe

--- src/main/java/engine/RoundRobinLB.java
+++ src/main/java/engine/RoundRobinLB.java
@@ -8,22 +8,25 @@ public class RoundRobinLB implements Loadbalanceing, Compute {
     private Queue<Compute> computingServers = new ArrayDeque<>();
 
     @Override
-    public void register(Compute stub) throws RemoteException {
+    public synchronized void register(Compute stub) throws RemoteException {
         computingServers.add(stub);
     }
 
     @Override
-    public void unregister(Compute stub) throws RemoteException {
+    public synchronized void unregister(Compute stub) throws RemoteException {
         computingServers.remove(stub);
     }
 
     @Override
     public <T> T executeTask(Task<T> t) throws RemoteException {
-        if(computingServers.size() == 0) {
-            return null; //TODO vllt. ändern. Problem wenn es keine Server zum bearbeiten der Anfrage gibt. vllt. den Loadbalancer die Anfrage machen lassen
+        if(computingServers.size() != 0) {
+            Compute a = null;
+            synchronized (this) {
+                a = computingServers.poll();
+                computingServers.add(a);
+            }
+            return a.executeTask(t);
         }
-        Compute a = computingServers.poll();
-        computingServers.add(a);
-        return a.executeTask(t);
+        return null; //TODO vllt. ändern. Problem wenn es keine Server zum bearbeiten der Anfrage gibt. vllt. den Loadbalancer die Anfrage machen lassen
     }
 }
